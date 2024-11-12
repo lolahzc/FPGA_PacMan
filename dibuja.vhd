e275ea540ac51204ff166379a28daefcc0992684
TheFrankerBoy <70015682+TheFrankerBoy@users.noreply.github.com>
2024-11-12 12:18:58 +0100

    Update dibuja.vhd
    Segunda Versión

--- dibuja.vhd
+++ dibuja.vhd
@@ -9,50 +9,50 @@ entity dibuja is
            eje_y : in STD_LOGIC_VECTOR (9 downto 0);
            
            codigo_color : in std_logic_vector (2 downto 0);
-           
-           RED : out STD_LOGIC_VECTOR (3 downto 0);
-           GRN : out STD_LOGIC_VECTOR (3 downto 0);
-           BLU : out STD_LOGIC_VECTOR (3 downto 0));
+           RGB : out STD_LOGIC_VECTOR (11 downto 0));
+
 end dibuja;
 
 architecture Behavioral of dibuja is
 signal RED_in, GRN_in, BLU_in : std_logic_vector (3 downto 0);
+signal REDaux, GRNaux, BLUaux : std_logic_vector (3 downto 0);
 
 begin
-    dibuja: process(eje_x, eje_y, codigo_color)
+    dibuja: process(eje_x, eje_y,codigo_color)
     begin
 
         case codigo_color is
             when "000" => -- Negro
-                RED <= "0000";
-                BLU <= "0000";
-                GRN <= "0000";
+                REDaux <= "0000";
+                BLUaux<= "0000";
+                GRNaux <= "0000";
             
             when "001" => -- Blanco
-                RED <= "1111";
-                BLU <= "1111";
-                GRN <= "1111";
+                REDaux <= "1111";
+                BLUaux<= "1111";
+                GRNaux <= "1111";
 
             when "010" => -- Amarillo
-                RED <= "1111";
-                BLU <= "1111";
-                GRN <= "0000";
+                REDaux <= "1111";
+                BLUaux<= "1111";
+                GRNaux <= "0000";
 
             when "011" => -- Rojo 
-                RED <= "1111";
-                BLU <= "0000";
-                GRN <= "0000";
+                REDaux <= "1111";
+                BLUaux<= "0000";
+                GRNaux <= "0000";
                 
             when others => 
-                RED <= "0000";
-                BLU <= "0000";
-                GRN <= "0000";
+                REDaux <= "0000";
+                BLUaux<= "0000";
+                GRNaux <= "0000";
                 
             end case;
     end process;
 
-RED <= std_logic_vector(RED_in);
-GRN <= std_logic_vector(GRN_in);
-BLU <= std_logic_vector(BLU_in);
+--RED <= std_logic_vector(RED_in);
+--GRN <= std_logic_vector(GRN_in);
+--BLU <= std_logic_vector(BLU_in);
 
+RGB <= REDaux & BLUaux & GRNaux;
 end Behavioral;
