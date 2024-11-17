1a7aa765cf87e641ce166c665ee0b012e3bde571
TheFrankerBoy <70015682+TheFrankerBoy@users.noreply.github.com>
2024-11-17 03:16:01 +0100

    Update MaqPACMAN.vhd
    Se ha mejorado el proceso síncrono y combinacional, 
    Ya se mueve el comecocos por la pantalla, sin embargo, tenemos que hacer que no deje una estela amarilla detrás y que no se choque con los muros.

--- MaqPACMAN.vhd
+++ MaqPACMAN.vhd
@@ -22,12 +22,15 @@ entity MaqPACMAN is
 end MaqPACMAN;
 
 architecture Behavioral of MaqPACMAN is
-    type estados is (reposo, botonDireccion, movimiento, muroEnMov, muroNoMov, movDir, botonDireccion2, noMov);
+    type estados is (reposo, botonDireccion, movimiento, muroEnMov, muroNoMov, movDir, botonDireccion2, noMov,pintaPacman);
     signal estado, p_estado    : estados;
-    signal posx, p_posx        : std_logic_vector(3 downto 0);
-    signal posy, p_posy        : std_logic_vector(4 downto 0);
+    signal p_address,last_address          : std_logic_vector(8 downto 0);
+    signal posx, p_posx, posx_ant     : std_logic_vector(3 downto 0);
+    signal posy, p_posy, posy_ant        : std_logic_vector(4 downto 0);
+    signal p_Dout : std_logic_vector(2 downto 0);
     signal last_udlr, p_last_udlr : std_logic_vector(3 downto 0);
-    signal pacmanEnMov         : std_logic;  -- Signal indicating if PacMan is in motion
+    signal pacmanEnMov: std_logic;  -- Signal indicating if PacMan is in motion
+    signal p_write  : std_logic_vector(0 downto 0);
     signal done_reg : std_logic  := '0'; -- Register done signal  
 
 begin
@@ -41,27 +44,29 @@ begin
             posy <= (others => '0');
             last_udlr <= (others => '0');
             done <= '0';
-            write <= "0";
             done_reg <= '0';
             pacmanEnMov <= '0';
             done_reg <= '0';
        
         elsif rising_edge(clk) then
+        
+            addressAOut <= p_address;
+            dAOut <= p_Dout;
             estado <= p_estado;
             posx <= p_posx;
             posy <= p_posy;
+            write <= p_write;   
             last_udlr <= p_last_udlr;
 
-            if p_estado = movimiento then
+            if (p_estado = movimiento) then
                 pacmanEnMov <= '1';
             else
                 pacmanEnMov <= '0';
             end if; 
 
             done <= done_reg ;
-            done_reg <= '0'; -- Desactivate 'done_reg' in each refresh
+          
             
-            write <= "0";
 
         end if;
     end process;
@@ -69,74 +74,96 @@ begin
     comb: process(estado, refresh, move, udlrIn, dAIn, p_posx, p_posy, posx, posy, pacmanEnMov)
     begin
         -- Default outputs
-        dAOut <= "000";
-        enableMem <= '1';
-        write <= "0";
+        p_Dout <= "000";
+        p_write <= "0";
+       
         p_estado <= estado;
-        addressAOut <= (others => '0');
+        p_address <= p_posx & p_posy;
         p_posx <= posx;
-        p_posy <= posy;    
+        p_posy <= posy;
+        enableMem <= '1';  
+        done_reg <= '0';
+             
 
     
         case estado is
             when reposo =>
-                if move = '1' and refresh = '1' then
+                if move = '1'  then
                     p_posx <= "0001";  -- Initial X position
                     p_posy <= "00001"; -- Initial Y position
-                    dAOut <= "011";    -- Draw Pac-Man
-                    write <= "1";
+                    p_Dout <= "011";    -- Draw Pac-Man
+                    p_address <= p_posx & p_posy;
+                    p_write <= "1";
                     p_estado <= botonDireccion;
                 end if;
 
             when botonDireccion =>
-                if refresh = '1' then
+                posx_ant <= posx;
+                posy_ant <= posy;
+                p_write <="1";
+                
                     if udlrIn = "1000" then  -- UP direction
-                        addressAOut <= std_logic_vector(unsigned(p_posy) - 1) & posx;
+                    
+                        p_posx <= std_logic_vector(unsigned(posx) - 1);
+                        p_posy <= posy;
                         p_estado <= movimiento;
                     elsif udlrIn = "0100" then  -- DOWN direction
-                        addressAOut <= std_logic_vector(unsigned(p_posy) + 1) & posx;
+                        p_posx <= std_logic_vector(unsigned(posx) + 1);
+                        p_posy <= posy;
                         p_estado <= movimiento;
                     elsif udlrIn = "0010" then  -- LEFT direction
-                        addressAOut <= p_posy & std_logic_vector(unsigned(posx) - 1);
+                        p_posx <= posx;
+                        p_posy <= std_logic_vector(unsigned(posy) - 1);
                         p_estado <= movimiento;
                     elsif udlrIn = "0001" then  -- RIGHT direction
-                        addressAOut <= p_posy & std_logic_vector(unsigned(posx) + 1);
+                        p_posx <= posx;
+                        p_posy <= std_logic_vector(unsigned(posy) + 1);
                         p_estado <= movimiento;
                     else
                         p_estado <= reposo;
                     end if;
-                end if;
+        
 
             when movimiento =>
-                if pacmanEnMov = '1' then
-                    p_estado <= muroEnMov;
-                    done_reg <= '1';    -- Correct movement
+                if (pacmanEnMov = '1' ) then
                     
-                    addressAOut <= p_posx & p_posy ;
-                    dAOut <= "000";
-                    write <= "1";
-                    addressAOut <= posx & posy; 
-                    dAOut <= "011";     -- As it's a correct movement draw PacMan
-                    write <= "1";       -- Activate write to draw 
-                else
+                    p_address <= posx_ant & posy_ant ;
+                    p_Dout <= "000"; --Dibujo pacman en la posición siguiente
+                    p_write <="1";
+                    --Me voy a la pos anterior y pinto vacío
+                    done_reg <= '1';     
+                    p_estado <= pintaPacman; --
+                    
+                    elsif( Refresh = '1' )        then
                     p_estado <= muroNoMov;
-                end if;
-
+                    end if;
+            when pintaPacman =>
+               
+                p_address <= p_posx & p_posy;
+                p_Dout <= "011";
+                p_write <="1";
+                if(Refresh = '1' ) then
+                
+                p_estado <= muroEnMov;
+                else
+                p_estado <= estado;
+               end if;
+             
             when muroEnMov =>
                 if  (udlrIn = "1000" and dAIn = "001") or 
                     (udlrIn = "0100" and dAIn = "001") or 
                     (udlrIn = "0010" and dAIn = "001") or 
                     (udlrIn = "0001" and dAIn = "001") then  -- Wall detected
-                    p_estado <= botonDireccion;              -- Maintain direction
+                    p_estado <= muroEnMov;              --Si detecta un muro no hace nada
                 else
                     p_estado <= botonDireccion2;             -- Move in button direction
                 end if;
 
             when botonDireccion2 =>
-                if move = '1' and refresh = '1' then  
-                    p_estado <= muroEnMov;
+                if move = '1'  then  
+                    p_estado <= botonDireccion; --Si le siguen llegando señales de movimiento, se va al principio
                 else
-                    p_estado <= noMov;
+                    p_estado <= noMov; --Si no tiene señales, que no se mueva
                 end if;
 
             when muroNoMov =>
@@ -151,8 +178,9 @@ begin
 
             when noMov =>
                 -- Stop movement and reset to initial direction
-                write <= "0";
-                p_estado <= reposo;
+                p_write <= "0";
+                p_estado <= botonDireccion2;
+                
 
             when others =>
                 p_estado <= reposo;
